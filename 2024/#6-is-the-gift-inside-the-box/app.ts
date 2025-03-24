7c5a3d60d77b15374a7e77a7c44a54d3b83ca486
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-03-24 14:16:58 +0330

    just prettier

--- 2024/#6-is-the-gift-inside-the-box/app.ts
+++ 2024/#6-is-the-gift-inside-the-box/app.ts
@@ -2,57 +2,29 @@
 
 // fun fact: I was trying to do this *without RegEx* but seems like the RegEx approach is one of the best approaches! thanks to this repo: https://github.com/hozlucas28/AdventJS-Solutions-2024
 function inBox(box: string[]): boolean {
-  const regex: RegExp = new RegExp(/^# *\* *#$/g)
+  const regex: RegExp = new RegExp(/^# *\* *#$/g);
 
-	let i: number
-	let line: string
+  let i: number;
+  let line: string;
 
-	for (i = 1; i < box.length - 1; i++) {
-		line = box[i]
-		if (regex.test(line)) return true
-	}
+  for (i = 1; i < box.length - 1; i++) {
+    line = box[i];
+    if (regex.test(line)) return true;
+  }
 
-	return false
+  return false;
 }
 
-console.log(inBox([
-  "###", 
-  "#*#", 
-  "###"
-])); // ➞ true
+console.log(inBox(["###", "#*#", "###"])); // ➞ true
 
 // my additional test btw
-console.log(inBox([
-  "#*#", 
-  "# #", 
-  "###"
-])); // ➞ false
+console.log(inBox(["#*#", "# #", "###"])); // ➞ false
 
 // my second additional test btw
-console.log(inBox([
-  "###", 
-  "# #", 
-  "#*#"
-])); // ➞ false
+console.log(inBox(["###", "# #", "#*#"])); // ➞ false
 
-console.log(inBox([
-  "####", 
-  "#* #", 
-  "#  #", 
-  "####"
-])); // ➞ true
+console.log(inBox(["####", "#* #", "#  #", "####"])); // ➞ true
 
-console.log(inBox([
-  "#####", 
-  "#   #", 
-  "#  #*", 
-  "#####"
-])); // ➞ false
+console.log(inBox(["#####", "#   #", "#  #*", "#####"])); // ➞ false
 
-console.log(inBox([
-  "#####", 
-  "#   #",
-  "#   #", 
-  "#   #", 
-  "#####"
-])); // ➞ false
+console.log(inBox(["#####", "#   #", "#   #", "#   #", "#####"])); // ➞ false
